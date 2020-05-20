6a30ac44fe0dc46d23537dc679bbccb88ad52902
kagi714 <kagi8kagi0@gmail.com>
2020-05-21 01:44:10 +0900

    Vectorからangleを削除

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+import math
 import pyxel
 
 #[[img, u, v, w, h, colkey], ...]
@@ -8,18 +9,18 @@ SHIP_IMGS   = [[ 0,  0,  0,  7,  7,  0]]
 SHIP_TIMS   = [10]
 
 class Vector():
-    def __init__(self, x=0.0, y=0.0, r=0.0):
+    def __init__(self, x=0.0, y=0.0):
         self.x = x
         self.y = y
-        self.angle = r
 
     def update(self, vel):
         self.x += vel.x
         self.y += vel.y
-        self.angle += vel.angle
 
-    def data(self):
-        return self.x, self.y, self.angle
+    def rotate(self, theta):
+        x, y = self.x, self.y
+        self.x = x * math.cos(theta) - y * math.sin(theta)
+        self.y = x * math.sin(theta) + y * math.cos(theta)
 
 class Anim():
     def __init__(self, imgs, tims):
@@ -34,9 +35,8 @@ class Anim():
     def draw(self, pos):
         img = self.__images[self.__index]
 
-        px, py, _ = pos.data()
-        x = px + (img[3]/2)
-        y = py + (img[4]/2)
+        x = pos.x + (img[3]/2)
+        y = pos.y + (img[4]/2)
 
         pyxel.blt(x ,y ,img[0], img[1], img[2], img[3], img[4], img[5])
 
@@ -49,22 +49,30 @@ class Anim():
                 self.__index += 1
 
 class Bullet():
-    def __init__(self, pos, anim):
+    def __init__(self, pos, rot, anim):
         self.__pos = pos
+        self.__rot = rot
         self.__anim = anim
-        self.__vel = Vector(0.0,1.3,0.0)
+        self.__vel = Vector(0.0,1.3)
 
     def update(self):
+        self.__go_forward(self.__rot)
         self.__pos.update(self.__vel)
 
     def draw(self):
         self.__anim.draw(self.__pos)
 
+    def __go_forward(self, theta):
+        self.__vel.x = 0
+        self.__vel.y = 1.3
+        self.__vel.rotate(theta)
+
 class Player():
-    def __init__(self, pos, anim):
+    def __init__(self, pos, rot, anim):
         self.__pos = pos
+        self.__rot = rot
         self.__anim = anim
-        self.__vel = Vector(0.0,0.0,0.0)
+        self.__vel = Vector(0.0,0.0)
 
     def update(self):
         self.__control()
@@ -75,26 +83,23 @@ class Player():
 
     def __control(self):
         vx, vy = 0.0, 0.0
-        if pyxel.btn(pyxel.KEY_A) :
-            vx = -1.0
-        if pyxel.btn(pyxel.KEY_D) :
-            vx =  1.0
-        if pyxel.btn(pyxel.KEY_W) :
-            vy = -1.0
-        if pyxel.btn(pyxel.KEY_S) :
-            vy =  1.0
+        if pyxel.btn(pyxel.KEY_A) : vx = -1.0
+        if pyxel.btn(pyxel.KEY_D) : vx =  1.0
+        if pyxel.btn(pyxel.KEY_W) : vy = -1.0
+        if pyxel.btn(pyxel.KEY_S) : vy =  1.0
         self.__vel.x = vx
         self.__vel.y = vy
 
-def new_object(type, vec = None):
+def new_object(type, vec = None ,theta = None):
     pos = vec if vec is not None else Vector(0.0,0.0)
+    rot = theta if theta is not None else 0
 
     if type == "Player":
         anim = Anim(SHIP_IMGS, SHIP_TIMS)
-        return Player(pos, anim)
+        return Player(pos, rot, anim)
     elif type == "Bullet":
         anim = Anim(BULLET_IMGS, BULLET_TIMS)
-        return Bullet(pos, anim)
+        return Bullet(pos, rot, anim)
     else:
         raise
 
@@ -108,7 +113,7 @@ class App():
         self.objs.append(new_object("Player",pos))
 
         pos = Vector(20.0, 10.0)
-        self.objs.append(new_object("Bullet",pos))
+        self.objs.append(new_object("Bullet",pos,math.pi/6.0))
 
         pyxel.run(self.update, self.draw)
 

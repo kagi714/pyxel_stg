31bedacfece87cfb0508a347f3fed87724ad1ced
kagi714 <kagi8kagi0@gmail.com>
2020-05-31 02:15:26 +0900

    当たり判定処理を追加

--- main.py
+++ main.py
@@ -27,13 +27,27 @@ class Vector():
         self.x = x * math.cos(theta) - y * math.sin(theta)
         self.y = x * math.sin(theta) + y * math.cos(theta)
 
-class Collision():
-    def __init__(self, siz=0.0, type=0x00):
-        self.__size = siz
-        self.__type = type
+    def distance(self, vec):
+        return math.sqrt((self.x-vec.x)**2+(self.y-vec.y)**2)
 
-    def is_hit_to():
-        pass
+class Collision():
+    def __init__(self, siz=0.0, type=0x00, onhit_func=None):
+        self.size = siz
+        self.type = type
+        self.__onhit = onhit_func
+
+    def update(self, objs, pos):
+        for o in objs:
+            opos, ocol = o.get_hitbox()
+            if self.__is_hit( pos, opos, ocol):
+                self.__onhit(o)
+
+    def __is_hit(self, selfpos, objpos, objcol):
+        if self.type & objcol.type:
+            if selfpos.distance(objpos) < (self.size+objcol.size):
+                return True
+        return False
+
 
 class Anim():
     def __init__(self, imgs, tims):
@@ -62,10 +76,13 @@ class Anim():
                 self.__index += 1
 
 class Bullet():
-    def __init__(self, pos, rot, anim):
+    def __init__(self, app, pos, rot, anim):
+        self.__app = app
         self.__pos = pos
         self.__rot = rot
         self.__anim = anim
+
+        self.__col = Collision(2.0, 0x01, self.__on_hit)
         self.__vel = Vector(0.0,1.3)
 
     def update(self):
@@ -75,24 +92,40 @@ class Bullet():
     def draw(self):
         self.__anim.draw(self.__pos)
 
+    def get_hitbox(self):
+        return self.__pos, self.__col
+
+    def __on_hit(self, obj):
+        pass
+
     def __go_forward(self, theta):
         self.__vel.x = 0
-        self.__vel.y = 1.3
+        self.__vel.y = 0.3
         self.__vel.rotate(theta)
 
 class Player():
-    def __init__(self, pos, rot, anim):
+    def __init__(self, app, pos, rot, anim):
+        self.__app = app
         self.__pos = pos
         self.__rot = rot
         self.__anim = anim
+
+        self.__col = Collision(7.0, 0x0F, self.__on_hit)
         self.__vel = Vector(0.0,0.0)
 
     def update(self):
         self.__control()
         self.__pos.update(self.__vel)
+        self.__col.update(self.__app.get_hitobjects(self), self.__pos)
 
     def draw(self):
         self.__anim.draw(self.__pos)
+        
+    def get_hitbox(self):
+        return self.__pos, self.__col
+
+    def __on_hit(self, obj):
+        print("hit")
 
     def __control(self):
         vx, vy = 0.0, 0.0
@@ -132,11 +165,17 @@ class App():
 
         if type == "Player":
             anim = Anim(SHIP_IMGS, SHIP_TIMS)
-            return Player(pos, rot, anim)
+            return Player(self ,pos, rot, anim)
         elif type == "Bullet":
             anim = Anim(BULLET_IMGS, BULLET_TIMS)
-            return Bullet(pos, rot, anim)
+            return Bullet(self, pos, rot, anim)
         else:
             raise
+
+    def get_hitobjects(self, obj):
+        exclude_self  = lambda o : o is not obj
+        is_has_hitbox = lambda o : o.get_hitbox() is not None
+
+        return filter(lambda o : exclude_self(o) & is_has_hitbox(o), self.objs)
 
 App()
